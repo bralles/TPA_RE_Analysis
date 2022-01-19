2d72aa3756cabc3627566059fb6b16cae65ba219
Alex Salas <bralles2175@gmail.com>
2022-01-18 21:07:04 -0500

    added info to README.md

--- re_.py
+++ re_.py
@@ -1,5 +1,16 @@
 # Our first python file. 
 
-a = input("Please type your name: ").strip().title()
-output = "Hello, {}! Enjoy working on the rest of this project.".format(a)
-print(output)
+import requests
+
+url = "https://zillow-com1.p.rapidapi.com/propertyExtendedSearch"
+
+querystring = {"location":"33625","home_type":"Houses"}
+
+headers = {
+    'x-rapidapi-host': "zillow-com1.p.rapidapi.com",
+    'x-rapidapi-key': "659f0d67e7msh23c12f2b1bf79d3p101746jsn01f238cd8a82"
+    }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(response.text)
