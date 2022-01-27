38c210ece76d8c4d444980835fac86628d75be75
Alex Salas <bralles2175@gmail.com>
2022-01-27 18:28:22 -0500

    new endpoint and pandas installed

--- 19_.py
+++ 19_.py
@@ -2,11 +2,12 @@
 Lets capture COVID cases for plotting. Here is a site that shows you how to use the API
 https://rapidapi.com/blog/covid-19-data-api-with-python-php-ruby-javascript-examples/
 
-All functions created will end with _19 as naming convention.
+We can use this page as a guide for python syntax: 
+https://www.w3schools.com/python/default.asp
 
 '''
 import mod_19
 
 # Will use our custom function to make a call and get covid-19 data.
-mod_19.get_covid_data_19()
+mod_19.get_covid_data_19("Italy", "2022-01-24")
 
--- mod_19.py
+++ mod_19.py
@@ -6,21 +6,52 @@ one that we created for us.
 '''
 import requests
 
-def get_covid_data_19():
+def get_covid_data_19(country_name, date):
     '''
-    This function gets COVID 19 data via rapid API. 
+    This function gets COVID 19 data via rapid API. This takes two parameters: 
+    Country Name (string) and Date (YYYY-MM-DD)
+    API info and config: https://rapidapi.com/Gramzivi/api/covid-19-data/
 
     '''
+    import pandas as pd
+    c = country_name
+    d = date
     # Endpoint that we are going to be hitting:
-    url = "https://covid-19-data.p.rapidapi.com/totals"
-    # credentials:
+    url = "https://covid-19-data.p.rapidapi.com/report/country/name"
+
+    querystring = {"name":c,"date":d}
+
     headers = {
         'x-rapidapi-host': "covid-19-data.p.rapidapi.com",
         'x-rapidapi-key': "659f0d67e7msh23c12f2b1bf79d3p101746jsn01f238cd8a82"
         }
-    # Request for data
-    response = requests.request("GET", url, headers=headers)
-    json_data = response.text
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    json = response.json()
+    df = pd.json_normalize(json[0])
+    print(df)
+
+    '''
+    #TODO 
+    This needs to be recode in order to strip out the following sample response: 
+
+    [1 item
+    0:{5 items
+    "country":"Italy"
+    "provinces":[1 item
+    0:{5 items
+    "province":"Italy"
+    "confirmed":110574
+    "recovered":16847
+    "deaths":13155
+    "active":80572
+    }
+    ]
+    "latitude":41.87194
+    "longitude":12.56738
+    "date":"2020-04-01"
+    }
+    ]
 
     #clean it
     remove_symbols = ["[", "]", "{", "}"]
@@ -38,3 +69,4 @@ def get_covid_data_19():
 
     #pretty print
     print("{}\n{}\n{}\n{}".format(confirmed, recovered, critical, deaths).replace('"', ""))
+    '''
